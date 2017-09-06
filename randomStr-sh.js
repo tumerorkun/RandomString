fb89bf919f200f30e96b56286e4944179f7aa6c1
Orkun Tumer <orkun.t@SARKI.LOCAL>
2017-09-06 17:39:18 +0300

    chars minified

--- randomStr-sh.js
+++ randomStr-sh.js
@@ -1,7 +1,7 @@
 try{
   const fs = require('fs');
   fs.writeFileSync('random.txt','');
-  const chars = ["a", "b", "c", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z", "A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z", "0", "1", "2", "3", "4", "5", "6", "7", "8", "9"];
+  const chars = ["a","b","c","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z","A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z","0","1","2","3","4","5","6","7","8","9"];
   var ids=0, sayac=0, final = [],
       komb = { min:6, max:20, bMin:0, bMax:6 };
       
