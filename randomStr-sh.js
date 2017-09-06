888a1c95bd01a570b2c2885e2ca1c3b1fabb4898
Orkun Tumer <orkun.t@SARKI.LOCAL>
2017-09-06 18:06:32 +0300

    Beautify

--- randomStr-sh.js
+++ randomStr-sh.js
@@ -1,10 +1,11 @@
 try{
   const fs = require('fs');
-  fs.writeFileSync('random.txt','');
   const chars = ["a","b","c","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z","A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z","0","1","2","3","4","5","6","7","8","9"];
-  var ids=0, sayac=0, final = [],
-      komb = { min:6, max:20, bMin:0, bMax:6 };
-      
+  var ids=0, sayac=0, final = [], fileName = 'RandomStrings'
+      komb = { min:6, max:20, syCount:400000, bMax:5 };
+  
+  fs.writeFileSync(fileName+'.txt','');
+
   function shuffle(a) {
     for (let p = a.length; p; p--) {
       let j = Math.floor(Math.random() * p);
@@ -15,16 +16,16 @@ try{
   function kombinasyon(numArr, choose, blg) {
     var n = numArr.length,
         c = [],
-        basla = function(start, choose_) {
-          if (choose_ == 0) {
+        basla = function ( start, choose_ ) {
+          if ( choose_ == 0 ) {
             var arr = c.concat('');
             shuffle(arr);
             final.push(arr.join('')+'\r\n');
             sayac++;
           }
           else {
-            for (var i = start; i <= n - choose_; ++i) {
-              if(sayac == 100000){break;}
+            for ( var i = start; i <= n - choose_; ++i ) {
+              if ( sayac == komb.syCount ) { break; }
               c.push(numArr[i]);
               basla(i + 1, choose_ - 1);
               c.pop();
@@ -34,17 +35,17 @@ try{
     basla(blg, choose);
   }
 
-  function yazdir(){
+  function yazdir() {
     shuffle(final);
     var son = new Array();
-    for( var b = 0,l = final.length-1; b <= l; b++ ){
+    for ( var b = 0,l = final.length-1; b <= l; b++ ){
       son[b] = (ids+b+1)+','+final[b];
     }
-    fs.appendFileSync('random.txt',son.join(''));
+    fs.appendFileSync(fileName+'.txt',son.join(''));
   }
 
-  for ( var a = komb.bMin; a < komb.bMax; a++ ) {
-    for( var i = komb.min; i <= komb.max; i++ ){
+  for ( var a = 0; a < komb.bMax; a++ ) {
+    for ( var i = komb.min; i <= komb.max; i++ ){
       kombinasyon(chars,i,a);sayac=0;
     }
     yazdir();ids = ids+final.length;final=[];
